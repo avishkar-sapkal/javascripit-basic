ec66a580e983ec3bbf91c13a3102be9b3600b2a7
avishkar-sapkal <157634351+avishkar-sapkal@users.noreply.github.com>
2024-06-03 00:44:45 +0530

    Add files via upload

---
+++ Data-type.js
@@ -0,0 +1,18 @@
+// String
+let name ="Avishkar";
+console.log("This is STRING data-type: "+name);         //Avishkar
+
+// Number
+let age = 20;
+console.log("This is NUMBER data-type: "+age);          //20
+
+// Object
+const cricketer={
+    name: "Dhoni",
+    Jesry_no : 7
+};
+console.log("This is Object data-type: "+cricketer.name+" "+cricketer.Jesry_no);            //Dhoni 7
+
+//Array
+const car=["Mt15","Gt650","Mastang"];             
+console.log(car);                                   // ["Mt15","Gt650","Mastang"]
