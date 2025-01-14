a3854791011d4e1d450aa8c4fad4c6c9126437b5
avishkar-sapkal <157634351+avishkar-sapkal@users.noreply.github.com>
2025-01-14 11:17:35 +0530

    Update Variable.js

--- Variable.js
+++ Variable.js
@@ -13,3 +13,10 @@ console.log("The value of Let variable is "+v);                 //30
 // const
 const c=100;
 console.log("The value of Constant variable is "+c);            //100
+
+
+// var outside is still used
+{
+    var x = 1000;
+}
+console.log("Outside the var is still working",x);              //1000
